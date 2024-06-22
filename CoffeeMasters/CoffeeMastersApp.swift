42351e875d5e964791c76610a413b35f45063c8a
Tusharbecoding <tusharchopra02122002@gmail.com>
2024-06-22 15:39:49 +0530

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Tushar Chopra on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
